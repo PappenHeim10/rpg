033d59a7d8ef9da4bd0aab65c8995df0830c3d44
PappenHeim10 <cohenlooc@gmail.com>
2025-02-05 16:53:13 +0100

    kleine verbesserungen

--- js/script.js
+++ js/script.js
@@ -1,6 +1,27 @@
 const player = document.querySelector('#player');
 
 
-player.document.addEventListener('keydown', (event) => {
-    
+document.addEventListener('keydown', (event) => {
+    switch (event.key) {
+        case 'a':
+            player.style.left = `${parseInt(player.style.left, 10) - 10}px`;
+            console.log('ArrowLeft');
+            break;
+        case 'd':
+            player.style.left = `${parseInt(player.style.left, 10) + 10}px`;
+            console.log('ArrowRight');
+            break;
+            
+        case 's':
+            player.style.top = `${parseInt(player.style.top, 10) - 10}px`;
+            console.log('ArrowDown');
+            break;
+            
+        case 'w':
+            player.style.top = `${parseInt(player.style.top, 10) + 10}px`;
+            console.log('ArrowUp');
+            break;
+            
+        default:
+    }
 });
