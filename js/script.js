867c502985ff4c2496504cd0506ba6748764a3a4
PappenHeim10 <cohenlooc@gmail.com>
2025-03-06 00:45:18 +0100

    Update styles, add 'Show All Spells' button, and implement spell display functionality

--- js/script.js
+++ js/script.js
@@ -9,6 +9,8 @@ const controlls = document.querySelector('.controlls');
 
 var box2 = document.querySelector('.box2'); // Zwite test box wird nicht benutztz
 
+
+
 function player(name){ // wird noch nicht benutzt, kommt später
     this.name = name;
     this.health = 100;
@@ -27,19 +29,20 @@ const playerStats = { // Einfaches Objekt mit default werten
     energy : 5,
     level : 1,
 
-    allStats : function(){
+    allStats : function(){ // Eine Funtion um sich alle Werte ansehen zu können
         let allStats = "Leben: " + this.health + "<br>" +
             "Stärke: " + this.strength + "<br>" +
             "Verteidigung: " + this.defence + "<br>" +
             "Mana: " + this.mana + "<br>" +
             "Energie: " + this.energy + "<br>" +
             "Level: " + this.level + "<br>";
+        
         return allStats; // Rückgabe der statistiken als string
     },
 
-    allSpells : function(){
+    allSpells : function(){ // Alle sprüche falls es sie gibt
         if(this.hasOwnProperty('spells')){
-            let spellList = [];
+            let spellList = "";
             for(let i = 0; i < this.spells.length; i++){
                 spellList += this.spells[i];
                 spellList += "<br>";
@@ -60,6 +63,7 @@ function addMagic(){
     if(!playerStats.spells.includes("Feuer_Ball")){
         controlls.appendChild(document.createElement('button')).textContent = "Fire Ball"; // Button wird angelegt und dem html hinzugefügt
         controlls.removeChild(document.querySelector('#learn_magic')); // Button wird gelöscht)
+        playerStats.spells.push("Feuer Ball"); // Zauber wird hinzugefügt
     }
 }
 
@@ -90,4 +94,12 @@ function showAllStats(){
     }
 }
 
-
+function showAllSpells(){
+    if(!show){
+        box2.innerHTML = playerStats.allSpells();
+        show = true;
+    }else if(show){
+        box2.innerHTML = "";
+        show = false;
+    }
+}
